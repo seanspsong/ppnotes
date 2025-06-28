e8b4848655d05377cc77d3889a9d6f7840da62e1
Sean <song@sugrsugr.com>
2025-06-28 20:31:34 +0800

    Initial commit - PPNotes Xcode project

---
+++ PPNotes/PPNotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PPNotes
+//
+//  Created by Sean Song on 6/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PPNotes/PPNotes/PPNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PPNotesApp.swift
+//  PPNotes
+//
+//  Created by Sean Song on 6/28/25.
+//
+
+import SwiftUI
+
+@main
+struct PPNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
